19be829af30e7021140fd80a675b2349bcdc4cbe
sreenujava <sreenujava@users.noreply.github.com>
2016-09-25 14:54:49 +0530

    new file1.java
    this is editing file

--- file.java
+++ file.java
@@ -3,5 +3,7 @@ class
 	public static void main(String[] args) 
 	{
 		System.out.println("hi this is git");
+		System.out.println("this is git programme")
+			System.out.println("your editing this file ")
 	}
 }
